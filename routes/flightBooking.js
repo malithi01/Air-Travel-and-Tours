095e2d63f3e8e938983fd1cbc574a207ece8a131
IT22886804 <chethanadassanayake747@gmail.com>
2025-03-20 00:06:45 +0530

    get, update and delete requests created

--- routes/flightBooking.js
+++ routes/flightBooking.js
@@ -17,4 +17,41 @@ router.post("/booking/save", async (req, res) => {
   }
 });
 
+// Get all booking details
+
+router.get("/booking", async (req, res) => {
+  try {
+    const newBooking = await Booking.find().exec();
+    return res.status(200).json({ success: true, existingRecords: newBooking });
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+});
+
+// Update rent details by ID
+router.put("/booking/update/:id", async (req, res) => {
+  try {
+    await Booking.findByIdAndUpdate(req.params.id, {
+      $set: req.body,
+    }).exec();
+    return res.status(200).json({ success: "Updated Successfully" });
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+});
+
+// Delete booking details by ID
+router.delete("/booking/delete/:id", async (req, res) => {
+  try {
+    const deletedBookings = await Booking.findByIdAndDelete(
+      req.params.id
+    ).exec();
+    return res.json({ message: "Delete Successfully", deletedBookings });
+  } catch (err) {
+    return res
+      .status(400)
+      .json({ message: "Deleted unsuccessfully", error: err.message });
+  }
+});
+
 module.exports = router;
