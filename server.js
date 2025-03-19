2ec51efb5d3a91083d46ef6fe84ee959a763c0c6
munch2001 <malithisandunika17@gmail.com>
2025-03-19 21:58:58 +0530

    Created model and route files for Car rentals

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
+//import express
 const express = require('express');
+
+//import mongoose
 const mongoose = require('mongoose');
+
+//import bodyParser-json file format convert to java script obj
+const bodyParser = require("body-parser");
+
+//import cors
+const cors = require('cors');
 
 const app = express();
 
+//import routes
+const carRoute = require("./routes/carRoute");
+
+//app middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+//route middleware
+app.use(carRoute);
+
+//declaring server running port and creating DB connection
 const PORT = 8000;
-const DB_URL = 'mongodb+srv://malithi01:travel01@airtravel.n87cr.mongodb.net/airTravel?retryWrites=true&w=majority';
+app.listen(PORT, () =>{
+    console.log(`App is running on ${PORT}`);
+});
+
+const DB_URL = 'mongodb+srv://malithi01:travel01@airtravel.n87cr.mongodb.net/airTravel';
 
 mongoose.connect(DB_URL,{
     useNewUrlParser: true,
-    useUniFiedTopology: true
+    useUnifiedTopology: true
 })
 .then(() => {
     console.log('DB connected');
@@ -16,6 +40,3 @@ mongoose.connect(DB_URL,{
 .catch((err) => console.log('DB connection error', err));
 
 
-app.listen(PORT, () =>{
-    console.log(`App is running on ${PORT}`);
-});
