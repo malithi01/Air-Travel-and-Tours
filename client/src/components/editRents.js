21658ce7da3d6cc1b0c2beb90954949f4625d0c9
munch2001 <malithisandunika17@gmail.com>
2025-03-27 20:37:05 +0530

    updated the editRents.js

--- client/src/components/editRents.js
+++ client/src/components/editRents.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
+import "../stylesheets/editRent.css";
 
 const EditRents = () => {
   const [carOrderid, setCarOrderId] = useState("");
   const [destination, setDestination] = useState("");
   const [vehicleType, setVehicleType] = useState("");
   const [pickUpLocation, setPickUpLocation] = useState("");
-  const [pickUpDate, setpickUpDate] = useState("");
+  const [pickUpDate, setPickUpDate] = useState("");
   const [dropOffLocation, setDropOffLocation] = useState("");
   const [dropOffDate, setDropOffDate] = useState("");
   const [formErrors, setFormErrors] = useState({});
@@ -16,32 +17,29 @@ const EditRents = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const getOneRent = async () => {
-      await axios
-        .get(`http://localhost:8000/rent/${id}`)
-        .then((res) => {
-          setCarOrderId(res.data.rentDetails.carOrderid);
-          setDestination(res.data.rentDetails.destination);
-          setVehicleType(res.data.rentDetails.vehicleType);
-          setPickUpLocation(res.data.rentDetails.pickUpLocation);
-          setpickUpDate(res.data.rentDetails.pickUpDate);
-          setDropOffLocation(res.data.rentDetails.dropOffLocation);
-          setDropOffDate(res.data.rentDetails.dropOffDate);
-          console.log(res.data.message);
-        })
-        .catch((err) => {
-          if (err.response) {
-            console.log(err.response.data.error);
-          } else {
-            console.log("Error occured while processing your get request");
-          }
-        });
-    };
-
-    getOneRent();
+    axios
+      .get(`http://localhost:8000/rent/${id}`)
+      .then((res) => {
+        const data = res.data.rentDetails;
+        setCarOrderId(data.carOrderid);
+        setDestination(data.destination);
+        setVehicleType(data.vehicleType);
+        setPickUpLocation(data.pickUpLocation);
+        setPickUpDate(
+          data.pickUpDate
+            ? new Date(data.pickUpDate).toISOString().slice(0, 16)
+            : ""
+        );
+        setDropOffLocation(data.dropOffLocation);
+        setDropOffDate(
+          data.dropOffDate
+            ? new Date(data.pickUpDate).toISOString().slice(0, 16)
+            : ""
+        );
+      })
+      .catch((err) => console.log("Error fetching data"));
   }, [id]);
 
-  // Validating form details
   const validateForm = () => {
     const errors = {};
     let formIsValid = true;
@@ -99,207 +97,128 @@ const EditRents = () => {
 
   const updateData = async (e) => {
     e.preventDefault();
+    if (!validateForm()) return;
 
-    if (!validateForm()) {
-      return;
-    }
+    const confirmed = window.confirm(
+      "Are you sure you want to update this Order?"
+    );
+    if (!confirmed) return;
 
-    try {
-      const confirmed = window.confirm(
-        "Are you sure you want to update this Order?"
-      );
-      if (confirmed) {
-        let updatedRentData = new FormData();
-        updatedRentData.append("carOrderid", carOrderid);
-        updatedRentData.append("destination", destination);
-        updatedRentData.append("vehicleType", vehicleType);
-        updatedRentData.append("pickUpLocation", pickUpLocation);
-        updatedRentData.append("pickUpDate", pickUpDate);
-        updatedRentData.append("dropOffLocation", dropOffLocation);
-        updatedRentData.append("dropOffDate", dropOffDate);
+    const updatedRentData = {
+      carOrderid,
+      destination,
+      vehicleType,
+      pickUpLocation,
+      pickUpDate: new Date(pickUpDate).toISOString(),
+      dropOffLocation,
+      dropOffDate: new Date(dropOffDate).toISOString(),
+    };
 
-        await axios
-          .put(`http://localhost:8000/rent/update/${id}`, updatedRentData)
-          .then((res) => {
-            alert(res.data.success);
-            console.log(res.data.success);
-            navigate("/rentDetails");
-          })
-          .catch((err) => {
-            if (err.response) {
-              console.log(err.response.data.success);
-            } else {
-              console.log("Error occured while processing your put request");
-            }
-          });
-      } else {
-        alert("Update cancelled!");
-      }
-    } catch (err) {
-      console.log("Update failed!");
-    }
+    axios
+      .put(`http://localhost:8000/rent/update/${id}`, updatedRentData)
+      .then(() => {
+        alert("Updated Successfully!");
+        navigate("/rentDetails");
+      })
+      .catch(() => console.log("Update failed!"));
   };
 
   const getOneDayLaterDate = () => {
     const today = new Date();
-    today.setDate(today.getDate() + 1);
-    return today.toISOString().split("T")[0];
+    today.setDate(today.getDate() + 2);
+    return today.toISOString().slice(0, 16); 
   };
 
   return (
-    <div className="col-md-8 mt-4 mx-auto">
-      <div
-        className="card"
-        style={{
-          backgroundColor: "rgba(255, 255, 255, 0.7)",
-          borderRadius: "10px",
-          marginTop: "110px",
-          marginBottom: "20px",
-        }}
-      >
-        <h1
-          className="h3 mb-3 font-weight-normal"
-          style={{ textAlign: "center", marginTop: "10px" }}
-        >
-          Update Rent details
-        </h1>
-        <form
-          className="needs-validation"
-          style={{ marginLeft: "10px", marginRight: "10px" }}
-          noValidate
-          onSubmit={updateData}
-        >
-          <div className="form-group" style={{ marginBottom: "15px" }}>
-            <label style={{ marginBottom: "5px" }}>Order ID</label>
+    <div className="editcontainer">
+      <div className="editcard">
+        <h2 className="edittitle">Update Rent Details</h2>
+        <form onSubmit={updateData}>
+          <div className="editform-group">
+            <label>Order ID</label>
             <input
               type="text"
-              className={`form-control ${
-                formErrors.carOrderid && "is-invalid"
-              }`}
-              name="carOrderid"
-              onChange={(e) => setCarOrderId(e.target.value)}
               value={carOrderid}
-              required
+              onChange={(e) => setCarOrderId(e.target.value)}
+              className={formErrors.carOrderid ? "error" : ""}
             />
             {formErrors.carOrderid && (
-              <div className="invalid-feedback">{formErrors.carOrderid}</div>
+              <p className="editerror-message">{formErrors.carOrderid}</p>
             )}
           </div>
 
-          <div className="form-group" style={{ marginBottom: "15px" }}>
-            <label style={{ marginBottom: "5px" }}>Destination</label>
+          <div className="editform-group">
+            <label>Destination</label>
             <input
               type="text"
-              className={`form-control ${
-                formErrors.destination && "is-invalid"
-              }`}
-              name="destination"
-              onChange={(e) => setDestination(e.target.value)}
               value={destination}
-              required
+              onChange={(e) => setDestination(e.target.value)}
+              className={formErrors.destination ? "error" : ""}
             />
             {formErrors.destination && (
-              <div className="invalid-feedback">{formErrors.destination}</div>
+              <p className="editerror-message">{formErrors.destination}</p>
             )}
           </div>
 
-          <div className="form-group" style={{ marginBottom: "15px" }}>
-            <label className="col-sm-2 col-form-label"> Vehicle Type </label>
-            <div className="col-sm-8">
-              <select
-                className="form-select"
-                name="vehicleType"
-                value={vehicleType}
-                onChange={(e) => setVehicleType(e.target.value)}
-              >
-                <option value="">Select a Vehicle type</option>
-                <option value="Car">Car</option>
-                <option value="Van">Van</option>
-                <option value="Bike">Bike</option>
-              </select>
-            </div>
+          <div className="editform-group">
+            <label>Vehicle Type</label>
+            <select
+              value={vehicleType}
+              onChange={(e) => setVehicleType(e.target.value)}
+            >
+              <option value="">Select a Vehicle</option>
+              <option value="Car">Car</option>
+              <option value="Van">Van</option>
+              <option value="Bike">Bike</option>
+            </select>
           </div>
 
-          <div className="form-group" style={{ marginBottom: "15px" }}>
-            <label style={{ marginBottom: "5px" }}>Pick Up Location</label>
+          <div className="editform-group">
+            <label>Pick Up Location</label>
             <input
               type="text"
-              className={`form-control ${
-                formErrors.pickUpLocation && "is-invalid"
-              }`}
-              name="pickUpLocation"
-              onChange={(e) => setPickUpLocation(e.target.value)}
               value={pickUpLocation}
-              required
+              onChange={(e) => setPickUpLocation(e.target.value)}
             />
             {formErrors.pickUpLocation && (
-              <div className="invalid-feedback">{formErrors.pickUpLocation}</div>
+              <span className="error-text">{formErrors.pickUpLocation}</span>
             )}
           </div>
 
-          <div className="form-group" style={{ marginBottom: "15px" }}>
-            <label style={{ marginBottom: "5px" }}>Pick Up Date</label>
+          <div className="editform-group">
+            <label>Pick Up Date</label>
             <input
               type="datetime-local"
-              className={`form-control ${
-                formErrors.pickUpDate && "is-invalid"
-              } `}
-              name="pickUpDate"
               value={pickUpDate}
               min={getOneDayLaterDate()}
-              onChange={(e) => setpickUpDate(e.target.value)}
-              required
+              onChange={(e) => setPickUpDate(e.target.value)}
             />
             {formErrors.pickUpDate && (
-              <div className="invalid-feedback">
-                {formErrors.pickUpDate}
-              </div>
+              <span className="error-text">{formErrors.pickUpDate}</span>
             )}
           </div>
 
-          <div className="form-group" style={{ marginBottom: "15px" }}>
-            <label style={{ marginBottom: "5px" }}>Drop Off Location</label>
+          <div className="editform-group">
+            <label>Drop Off Location</label>
             <input
               type="text"
-              className={`form-control ${
-                formErrors.dropOffLocation && "is-invalid"
-              }`}
-              name="dropOffLocation"
-              onChange={(e) => setDropOffLocation(e.target.value)}
               value={dropOffLocation}
-              required
+              onChange={(e) => setDropOffLocation(e.target.value)}
             />
-            {formErrors.dropOffLocation && (
-              <div className="invalid-feedback">{formErrors.dropOffLocation}</div>
-            )}
           </div>
 
-          <div className="form-group" style={{ marginBottom: "15px" }}>
-            <label style={{ marginBottom: "5px" }}>Drop off Date</label>
+          <div className="editform-group">
+            <label>Drop Off Date</label>
             <input
-              type="date"
-              className={`form-control ${
-                formErrors.dropOffDate && "is-invalid"
-              } `}
-              name="dropOffDate"
+              type="datetime-local"
               value={dropOffDate}
-              min={new Date().toISOString().split("T")[0]}
+              min={pickUpDate ? new Date(new Date(pickUpDate).getTime() + 2).toISOString().slice(0, 16) : new Date().toISOString().slice(0, 16)}
               onChange={(e) => setDropOffDate(e.target.value)}
-              required
             />
-            {formErrors.dropOffDate && (
-              <div className="invalid-feedback">
-                {formErrors.dropOffDate}
-              </div>
-            )}
           </div>
 
-          <button
-            className="btn btn-success"
-            type="submit"
-            style={{ marginTop: "15px" }}
-          >
-            &nbsp;Update
+          <button type="submit" className="update-btn">
+            Update
           </button>
         </form>
       </div>
