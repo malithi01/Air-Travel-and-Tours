205b4b84ee689d86a809a18b6f9397876d6bd86c
munch2001 <malithisandunika17@gmail.com>
2025-04-12 11:48:55 +0530

    Added styles for the packing list

--- client/src/components/packingForm.js
+++ client/src/components/packingForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import "../stylesheets/packingAssistant.css";
 
 const PackingForm = () => {
   const [tripType, setTripType] = useState("");
